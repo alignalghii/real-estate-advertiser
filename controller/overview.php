2f40519b63eb81423797a6ef5e2f6acdd2f16a56
Endrey Márk <ungazik@gmail.com>
2019-01-21 21:45:09 +0100

    OverviewController: navigation and timeout links

--- controller/overview.php
+++ controller/overview.php
@@ -1,10 +1,25 @@
 <?php
 
+require 'Model.php';
+
 class OverviewController
 {
 	private $n;
 
 	public function __construct(int $n) {$this->n = $n;}
 
-	public function index() {}
+	public function index()
+	{
+		$numberOfFlats = Model::numberOfFlats();
+		$n = $this->n;
+		if (1 <= $n && $n <= $numberOfFlats) {
+			if ($n > 1)              echo '<a href="?n='.($n-1).'">Vissza</a>';
+			echo '<br/>';
+			if ($n < $numberOfFlats) echo '<a href="?n='.($n+1).'">Tovább</a>';
+			echo '<br/>';
+			echo '<a href="?n='.($n<$numberOfFlats ? $n+1 : 1).'">15s</a>';
+		} else {
+			echo 'Error: wrong index number for record';
+		}
+	}
 }
