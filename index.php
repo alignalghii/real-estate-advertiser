3f8b69ef1dee4ea5ebb58fb54190017f3e91df3b
Endrey Márk <ungazik@gmail.com>
2019-01-22 14:39:44 +0100

    Rooter: more resource-efficient and less prone to duplicate loading

--- index.php
+++ index.php
@@ -1,21 +1,21 @@
 <?php
-require 'controller/overview.php';
-require 'controller/details.php';
-require 'controller/error.php';
 
 $p = $_GET['p'] ?? 'overview';
 
 switch ($p) {
 	case 'overview':
+		require 'controller/overview.php';
 		$n = (int) ($_GET['n'] ?? 1);
 		$controller = new OverviewController($n);
 		break;
 	case 'details':
+		require 'controller/details.php';
 		$n = (int) ($_GET['n'] ?? 1);
 		$i = (int) ($_GET['i'] ?? 1);
 		$controller = new DetailsController($n, $i);
 		break;
 	default:
+		require 'controller/error.php';
 		$controller = new ErrorController("Error: No such page as [$p]");
 }
 
