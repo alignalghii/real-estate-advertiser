e3494e843484bb15ffc85d49382c885866c1f3f5
Endrey Márk <ungazik@gmail.com>
2019-01-22 14:41:29 +0100

    DetailsController passes tests

--- controller/details.php
+++ controller/details.php
@@ -1,5 +1,7 @@
 <?php
 
+require 'Model.php';
+
 class DetailsController
 {
 	private $n;
@@ -7,5 +9,27 @@ class DetailsController
 
 	public function __construct(int $n, int $i) {$this->n = $n; $this->i = $i;}
 
-	public function index() {}
+	public function index()
+	{
+		$n = $this->n;
+		$i = $this->i;
+		$numberOfFlats    = Model::numberOfFlats();
+		if (1 <= $n && $n <= $numberOfFlats) {
+			$pictures         = Model::picturesOfFlat($n);
+			$numberOfPictures = count($pictures);
+			if (1 <= $i && $i <= $numberOfPictures) {
+				echo '<link rel="stylesheet" type="text/css" href="gallery.css"/>';
+				echo '<ul>';
+				foreach ($pictures as $running => $picture) {
+					$class = $running+1 == $i ? 'big' : 'small';
+					echo "<li><img class=\"$class\" src=\"$n/{$pictures[$running]['filename']}\"/></li>";
+				}
+				echo '</ul>';
+			} else {
+				echo "Error: ";
+			}
+		} else {
+			echo "Error: ";
+		}
+	}
 }
