9d4fc59c1d17af48aff878fcbd1b702c514e316c
Endrey Márk <ungazik@gmail.com>
2019-01-25 09:15:07 +0100

    Valid HTML5 format

--- view/details.php
+++ view/details.php
@@ -1,23 +1,31 @@
-<link rel="stylesheet" type="text/css" href="gallery.css"/>
-<link rel="stylesheet" type="text/css" href="navigation.css"/>
-<a href="?p=overview&n=<?php echo $n; if ($i != 1) echo "&i=$i"; ?>">Áttekintéshez vissza</a>
-<br/>
-<a href="?p=overview&n=<?php echo $n; if ($i != 1) echo "&i=$i"; ?>">60s</a>
-<br/>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<link rel="stylesheet" type="text/css" href="gallery.css"/>
+		<link rel="stylesheet" type="text/css" href="navigation.css"/>
+		<title>Részletek</title>
+	</head>
+	<body>
+		<a href="?p=overview&n=<?php echo $n; if ($i != 1) echo "&i=$i"; ?>">Áttekintéshez vissza</a>
+		<br/>
+		<a href="?p=overview&n=<?php echo $n; if ($i != 1) echo "&i=$i"; ?>">60s</a>
+		<br/>
 <?php if ($i > 1): ?>
-	<a href="?p=details&n=<?php echo $n; ?>&i=<?php echo $i-1; ?>">Előző</a>
+		<a href="?p=details&n=<?php echo $n; ?>&i=<?php echo $i-1; ?>">Előző</a>
 <?php else: ?>
-	<span class="faint">Előző</span>
+		<span class="faint">Előző</span>
 <?php endif; ?>
-<br/>
+		<br/>
 <?php if ($i < $numberOfPictures): ?>
-	<a href="?p=details&n=<?php echo $n; ?>&i=<?php echo $i+1; ?>">Következő</a>
+		<a href="?p=details&n=<?php echo $n; ?>&i=<?php echo $i+1; ?>">Következő</a>
 <?php else: ?>
-	<span class="faint">Következő</span>
+		<span class="faint">Következő</span>
 <?php endif; ?>
-<ul>
-<?php foreach ($pictures as $running => $picture): 
-	$class = $running+1 == $i ? 'big' : 'small'; ?>
-	<li><img class="<?php echo $class; ?>" src="<?php echo "$n/{$pictures[$running]['filename']}"; ?>"/></li>
+		<ul>
+<?php foreach ($pictures as $running => $picture): $class = $running+1 == $i ? 'big' : 'small'; ?>
+			<li><img class="<?php echo $class; ?>" src="<?php echo "$n/{$pictures[$running]['filename']}"; ?>"/></li>
 <?php endforeach; ?>
-</ul>
+		</ul>
+	</body>
+</html>
--- view/overview.php
+++ view/overview.php
@@ -1,24 +1,33 @@
-<link rel="stylesheet" type="text/css" href="gallery.css"/>
-<link rel="stylesheet" type="text/css" href="navigation.css"/>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<link rel="stylesheet" type="text/css" href="gallery.css"/>
+		<link rel="stylesheet" type="text/css" href="navigation.css"/>
+		<title>Áttekintés</title>
+	</head>
+	<body>
 <?php if ($n > 1): ?>
-	<a href="?n=<?php echo $n-1; ?>">Előző</a>
+		<a href="?n=<?php echo $n-1; ?>">Előző</a>
 <?php else: ?>
-	<span class="faint">Előző</span>
+		<span class="faint">Előző</span>
 <?php endif; ?>
-<br/>
+		<br/>
 <?php if ($n < $numberOfFlats): ?>
-	<a href="?n=<?php echo $n+1; ?>">Következő</a>
+		<a href="?n=<?php echo $n+1; ?>">Következő</a>
 <?php else: ?>
-	<span class="faint">Következő</span>
+		<span class="faint">Következő</span>
 <?php endif; ?>
-<br/>
-<a href="?n=<?php echo $n<$numberOfFlats ? $n+1 : 1; ?>">15s</a>
-<br/>
-<a href="?p=details&n=<?php echo $n; ?>&i=<?php echo $i; ?>">Click for details!</a>
-<br/>
-<?php echo $flats[$n-1]['address']; ?>
-<ul>
+		<br/>
+		<a href="?n=<?php echo $n<$numberOfFlats ? $n+1 : 1; ?>">15s</a>
+		<br/>
+		<a href="?p=details&n=<?php echo $n; ?>&i=<?php echo $i; ?>">Click for details!</a>
+		<br/>
+		<?php echo $flats[$n-1]['address']; ?>
+		<ul>
 <?php foreach ($pictures as $i => $picture): ?>
-	<?php if ($i < 2): ?><li><img class="half" src="<?php echo "$n/{$pictures[$i]['filename']}"; ?>"/></li><?php endif; ?>
+			<?php if ($i < 2): ?><li><img class="half" src="<?php echo "$n/{$pictures[$i]['filename']}"; ?>"/></li><?php endif; ?>
 <?php endforeach; ?>
-</ul>
+		</ul>
+	</body>
+</html>
