69c741e65bcc56ae24e1becc6730425e8c2bbe05
Endrey Márk <ungazik@gmail.com>
2019-02-18 17:49:30 +0100

    Details page driven by JavaScript

--- pager.js
+++ pager.js
@@ -1,91 +1,114 @@
 domReady(pager);
 
-function pager()
-{
-	var modelData = document.getElementById('model').dataset;
-	var [n, i] = [modelData.n, modelData.i];
+function pager() {
+	document.addEventListener('click', clickHandler);
 
-	var [prevNav, nextNav] = [document.getElementById('prev'), document.getElementById('next')];
-	if (isEnabled(prevNav)) prevNav.addEventListener('click', goPrev);
-	if (isEnabled(nextNav)) nextNav.addEventListener('click', goNext);
+	var big = document.getElementById('big');
+	var prevNav = document.getElementById('prev'),
+	    nextNav = document.getElementById('next'),
+	    overNav = document.getElementById('over');
 
-	var focusIcon = document.getElementById('icon-'+n+'-'+i),
-	    prevIcon, nextIcon;
-	updatePrevIcon();
-	updateNextIcon();
 
-	var bigImg = document.getElementById('big');
-
-	function updatePrevIcon() {prevIcon  = focusIcon.parentElement.previousElementSibling ? focusIcon.parentElement.previousElementSibling.firstElementChild : null;}
-	function updateNextIcon() {nextIcon  = focusIcon.parentElement.nextElementSibling     ? focusIcon.parentElement.nextElementSibling.firstElementChild     : null;}
-
-	function goPrev(evt) {
-		evt.preventDefault();
-
-		if (i > 1) {
-			i--;
-			updateBigImg();
-			moveFocus(focusIcon, prevIcon);
-
-			// Recast roles:
-			[nextIcon, focusIcon]  = [focusIcon, prevIcon];
-			updatePrevIcon();
-
-			enable(nextNav, goNext);
-			if (!prevIcon) disable(prevNav, goPrev);
-			updateNavs();
+	function clickHandler(evt) {
+		var clickedImg = evt.target;
+		if (isUnfocusedIcon(clickedImg)) {
+			evt.preventDefault();
+			navigateBy(clickedImg);
+		} else {
+			var [isEnabledVertNav, direction, icon] = inspectEnabledVerticalNavigation(clickedImg);
+			if (isEnabledVertNav) {
+				evt.preventDefault();
+				switch (direction) {
+					case 'prev':
+						navigateBy(icon);
+						break;
+					case 'next':
+						navigateBy(icon);
+						break;
+				}
+			}
 		}
 	}
 
-	function goNext(evt) {
-		evt.preventDefault();
+	function navigateBy(icon) {
+		updateBigBy(icon);
+		fromFocus(icon);
+		updateNavButtonsBy(icon);
+	}
 
-		if (i < numberOfPictures) {
-			i++;
-			updateBigImg();
-			moveFocus(focusIcon, nextIcon);
+	function updateBigBy(icon) {
+		var src = icon.getAttribute('src');
+		big.setAttribute('src', src);
+	}
 
-			// Recast roles:
-			[prevIcon, focusIcon]  = [focusIcon, nextIcon];
-			updateNextIcon();
-
-			enable(prevNav, goPrev);
-			if (!nextIcon) disable(nextNav, goNext);
-			updateNavs();
+	function updateNavButtonsBy(icon) {
+		assignHref('overview', over, icon);
+		var [prevIcon, nextIcon] = neighborhood(icon);
+		if (prevIcon) {
+			assignHref('details', prevIcon, prevNav);
+			enableBy(prevNav, prevIcon);
+		} else {
+			disable(prevNav);
+		}
+		if (nextIcon) {
+			assignHref('details', nextIcon, nextNav);
+			enableBy(nextNav, nextIcon);
+		} else {
+			disable(nextNav);
 		}
 	}
-
-	function isEnabled(navElement) {return navElement.hasAttribute('href');}
-
-	function enable(navElement, handler) {
-		navElement.setAttribute('href', buildHref(n, i));
-		navElement.removeAttribute('class');
-		navElement.addEventListener('click', handler)
-	}
-
-	function disable(navElement, handler) {
-		navElement.removeAttribute('href');
-		navElement.className = 'faint';
-		navElement.removeEventListener('click', handler);
-	}
-
-	function updateNavs() {
-		if (isEnabled(prevNav) && i > 1               ) prevNav.setAttribute('href', buildHref(n, i-1));
-		if (isEnabled(nextNav) && i < numberOfPictures) nextNav.setAttribute('href', buildHref(n, i+1));
-	}
-
-	function updateBigImg() {bigImg.setAttribute('src', buildSrc(n, i));}
 }
 
-function buildHref(n, i) {return 'localhost:8000?p=details&n='+n+'&i='+i;}
-function buildSrc (n, i) {
-	var icon = document.getElementById('icon-'+n+'-'+i);
-	return icon.getAttribute('src');
+function neighborhood(icon) {return [prevCousin(icon), nextCousin(icon)];}
+function prevCousin(icon)         {return safeConjugate2('parentElement', 'firstElementChild', 'previousElementSibling', icon);}
+function nextCousin(icon)         {return safeConjugate2('parentElement', 'firstElementChild', 'nextElementSibling'    , icon);}
+function safeConjugate2(forward, backward, f, x) {return safeProp(safeProp(x[forward][forward][f], backward), backward);}
+function safeProp(ob, prop) {return ob ? ob[prop] : null;}
+
+function isUnfocusedIcon  (elt) {return elt.tagName == 'IMG' && elt.classList.contains('small') && !elt.classList.contains('small-focused');}
+function inspectEnabledVerticalNavigation(navLink) {
+	var direction = navLink.getAttribute('id');
+	var isVertNav = ['prev', 'next'].includes(direction) && navLink.getAttribute('href');
+	var focusIcon = document.getElementById('focus');
+	var [prevIcon, nextIcon] = neighborhood(focusIcon);
+	switch (direction) {
+		case 'prev':  icon = prevIcon; break;
+		case 'next':  icon = nextIcon; break;
+		default:      icon = null;
+	}
+	return [isVertNav, direction, icon];
+}
+
+function fromFocus(dst) {
+	var focus = document.getElementById('focus');
+	moveFocus(focus, dst);
 }
 
 function moveFocus(src, dst) {
-	if (dst) {
-		src.className="small";
-		dst.className="small-focused";
-	}
+	src.removeAttribute('id');
+	src.className = 'small';
+	dst.setAttribute('id', 'focus');
+	dst.className = 'small-focused';
+	var dstLink = dst.parentElement;
+	var srcLink = src.parentElement;
+	dstLink.removeAttribute('href');
+	assignHref('details', srcLink, src);
+}
+
+function assignHref(page, navLink, icon) {
+	var iconLinkData = icon.parentElement.dataset;
+	var n = iconLinkData.n,
+	    i = iconLinkData.i;
+	navLink.setAttribute('href', buildHref(page, n, i));
+}
+function buildHref(page, n, i) {return '?p='+page+'&n='+n+'&i='+i;}
+
+function enableBy(navLink, icon) {
+	assignHref('details', navLink, icon);
+	navLink.removeAttribute('class');
+}
+
+function disable(navLink) {
+	navLink.removeAttribute('href');
+	navLink.className = 'faint';
 }
--- view/details.php
+++ view/details.php
@@ -13,13 +13,13 @@
 		<ul class="navigation" id="model" data-n="<?php echo $n?>" data-i="<?php echo $i?>">
 			<li><a id="prev" <?php if ($prev): ?>href="?p=details&n=<?php echo $n; ?>&i=<?php echo $prev; ?>"<?php else: ?>class="faint"<?php endif; ?>>← Előző</a></li>
 			<li><a id="next" <?php if ($next): ?>href="?p=details&n=<?php echo $n; ?>&i=<?php echo $next; ?>"<?php else: ?>class="faint"<?php endif; ?>>→ Következő</a></li>
-			<li><a class="timer" data-interval="60" href="?p=overview&n=<?php echo $n; if ($i != $first) echo "&i=$i"; ?>">↑🌐 Áttekintéshez vissza</a></li>
+			<li><a id="over" class="timer" data-interval="60" href="?p=overview&n=<?php echo $n; if ($i != $first) echo "&i=$i"; ?>">↑🌐 Áttekintéshez vissza</a></li>
 		</ul>
 		<h1>Ingatlanhirdető | Részletek</h1>
 		<img class="big" id="big" src="<?php echo "$n/{$pictures[$i]['filename']}"; ?>"/>
 		<ul class="icon-line">
 <?php foreach ($pictures as $id => $picture): ?>
-			<li><img class="<?php echo $id == $i ? 'small-focused' : 'small'; ?>" id="icon-<?php echo $n; ?>-<?php echo $id; ?>" src="<?php echo "$n/{$picture['filename']}"; ?>"/></li>
+			<li><a data-n="<?php echo $n; ?>" data-i="<?php echo $id; ?>"<?php if ($id != $i): ?> href="<?php echo "?p=details&n=$n&i=$id"; ?>"<?php endif; ?>><img class="<?php echo $id == $i ? 'small-focused' : 'small'; ?>"<?php if ($id == $i): ?> id="focus"<?php endif; ?> src="<?php echo "$n/{$picture['filename']}"; ?>"/></a></li>
 <?php endforeach; ?>
 		</ul>
 	</body>
